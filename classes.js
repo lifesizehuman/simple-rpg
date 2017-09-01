6445a98ba76b1d8ce0832d333b432d541805d680
Slater Combes <combes.slater@gmail.com>
2017-08-31 22:56:26 -0700

    added modifiers to seperate file

---
+++ classes.js
@@ -0,0 +1,42 @@
+exports.modifiers = {
+  Warrior: {
+    hp: 100,
+    mana: 80,
+    offense: 120,
+    defense: 80,
+    speed: 80,
+    intelligence: 70
+  },
+  Mage: {
+    hp: 100,
+    mana: 150,
+    offense: 90,
+    defense: 120,
+    speed: 80,
+    intelligence: 100
+  },
+  Healer: {
+    hp: 130,
+    mana: 120,
+    offense: 30,
+    defense: 90,
+    speed: 50,
+    intelligence: 90
+  },
+  Berserker: {
+    hp: 90,
+    mana: 40,
+    offense: 150,
+    defense: 70,
+    speed: 100,
+    intelligence: 10
+  },
+  Warpriest: {
+    hp: 110,
+    mana: 120,
+    offense: 70,
+    defense: 80,
+    speed: 80,
+    intelligence: 90
+  }
+};
--- logic.js
+++ logic.js
@@ -1,48 +1,6 @@
 var inquirer = require("inquirer");
 
-// Put this into its own file later!
-var modifiers = {
-  Warrior: {
-    hp: 100,
-    mana: 80,
-    offense: 120,
-    defense: 80,
-    speed: 80,
-    intelligence: 70
-  },
-  Mage: {
-    hp: 100,
-    mana: 150,
-    offense: 90,
-    defense: 120,
-    speed: 80,
-    intelligence: 100
-  },
-  Healer: {
-    hp: 130,
-    mana: 120,
-    offense: 30,
-    defense: 90,
-    speed: 50,
-    intelligence: 90
-  },
-  Berserker: {
-    hp: 90,
-    mana: 40,
-    offense: 150,
-    defense: 70,
-    speed: 100,
-    intelligence: 10
-  },
-  Warpriest: {
-    hp: 110,
-    mana: 120,
-    offense: 70,
-    defense: 80,
-    speed: 80,
-    intelligence: 90
-  }
-};
+var classes = require("./classes.js");
 
 function Character(name, type) {
   var alive = "Alive";
@@ -51,13 +9,13 @@ function Character(name, type) {
   this.name = name;
   this.type = type;
   this.level = 1;
-  this.hp = this.level * modifiers[this.type].hp;
-  this.mana = this.level * modifiers[this.type].mana;
-  this.offense = this.level * modifiers[this.type].offense;
-  this.defense = this.level * modifiers[this.type].defense;
-  this.speed = this.level * modifiers[this.type].speed;
+  this.hp = this.level * classes.modifiers[this.type].hp;
+  this.mana = this.level * classes.modifiers[this.type].mana;
+  this.offense = this.level * classes.modifiers[this.type].offense;
+  this.defense = this.level * classes.modifiers[this.type].defense;
+  this.speed = this.level * classes.modifiers[this.type].speed;
   this.luck = Math.floor((Math.random() * 51) + 50);
-  this.intelligence = this.level * modifiers[this.type].intelligence;
+  this.intelligence = this.level * classes.modifiers[this.type].intelligence;
   this.status = function() {
     if (this.hp > 0) {
       console.log("Status: " + alive);
@@ -114,7 +72,11 @@ var createPlayer = function() {
       }, {
         type: "list",
         choices: [
-          "Warrior", "Mage", "Healer", "Berserker", "Warpriest"
+          "Warrior",
+          "Mage",
+          "Healer",
+          "Berserker",
+          "Warpriest"
         ],
         message: "Character Class: ",
         name: "type"
