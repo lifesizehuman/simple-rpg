d9af0cf7133e8acb3bb4eb636ce15faafe904435
Slater Combes <combes.slater@gmail.com>
2017-08-31 22:38:08 -0700

    normalize modifiers

--- logic.js
+++ logic.js
@@ -4,48 +4,43 @@ var inquirer = require("inquirer");
 var modifiers = {
   Warrior: {
     hp: 100,
-    mana: 10,
-    offense: 30,
+    mana: 80,
+    offense: 120,
     defense: 80,
     speed: 80,
-    luck: 50,
-    intelligence: 10
+    intelligence: 70
   },
   Mage: {
     hp: 100,
-    mana: 10,
-    offense: 30,
-    defense: 80,
+    mana: 150,
+    offense: 90,
+    defense: 120,
     speed: 80,
-    luck: 50,
-    intelligence: 10
+    intelligence: 100
   },
   Healer: {
-    hp: 100,
-    mana: 10,
+    hp: 130,
+    mana: 120,
     offense: 30,
-    defense: 80,
-    speed: 80,
-    luck: 50,
-    intelligence: 10
+    defense: 90,
+    speed: 50,
+    intelligence: 90
   },
   Berserker: {
-    hp: 100,
-    mana: 10,
-    offense: 30,
-    defense: 80,
-    speed: 80,
-    luck: 50,
+    hp: 90,
+    mana: 40,
+    offense: 150,
+    defense: 70,
+    speed: 100,
     intelligence: 10
   },
   Warpriest: {
-    hp: 100,
-    mana: 10,
-    offense: 30,
+    hp: 110,
+    mana: 120,
+    offense: 70,
     defense: 80,
     speed: 80,
-    luck: 50,
-    intelligence: 10
+    intelligence: 90
   }
 };
 
